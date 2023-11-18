42dc5ebb05251cdb26cd43ffa3bc4d698114a895
beneboy <bene@zipzop.de>
2023-11-18 23:57:27 +0100

    div

--- scripts/renameDirnames.php
+++ scripts/renameDirnames.php
@@ -1,68 +1,92 @@
 <?php
-  
-  $mydir = 'T:\MP3\_unsortiert';
-  $dryrun = true;
-  
-  while (true)
-  {
-    $dir_iterator = new RecursiveDirectoryIterator($mydir);
-    $iterator     = new RecursiveIteratorIterator($dir_iterator, RecursiveIteratorIterator::SELF_FIRST);
-
-    $breakit = false;
-
-    foreach ($iterator as $file) 
-    {
-      $file = trim($file);
-      
-      if (is_dir($file))
-      {        
-        $dirsplit = explode(DIRECTORY_SEPARATOR, $file);
-        $lastdir  = $dirsplit[ count($dirsplit) - 1 ];
-        
-        if ($lastdir === '.' || $lastdir === '..')
-          continue;
-        
-        unset($dirsplit[ count($dirsplit) - 1 ]);
-        $predir = implode(DIRECTORY_SEPARATOR, $dirsplit);
-        $predir = rtrim($predir, DIRECTORY_SEPARATOR);
-        
-        $nlastdir = substr($lastdir, 0, 40);
-        
-        $nlastdir = preg_replace('/[^äöüÄÖÜa-zA-Z0-9-_., ]/', '_', $nlastdir);
-        $nlastdir = preg_replace('/ {1,}/', ' ', $nlastdir);
-        $nlastdir = trim($nlastdir);
-
-        $new = $predir . DIRECTORY_SEPARATOR . $nlastdir;
-              
-        if ($file !== $new)
-        {
-          echo $file . "\n";
-          echo '--> ' . $new . "\n";   
-          
-          if ($dryrun === false)
-          {
-            $r = rename($file, $new);
-            var_dump($r);
-            
-            if ($r === true)
-            {
-              $breakit = true;
-              
-              usleep(200000);
-              
-              // DirSchleife neustarten, weil RecursiveIteratorIterator sonst Exception wirft
-              break;
-            }
-          }
-          
-          echo "\n"; 
-        }
-      }
-      
-    }
-    
-    if ($breakit === false)
-      break;  
-  }
+	
+	if (!isset($mydir))
+		$mydir = 'T:\MP3';
+	if (!isset($dryrun))
+		$dryrun = true;
+	if (!isset($maxlen))
+		$maxlen = 40;
+	if (!isset($errlog))
+		$errlog = __DIR__ . DIRECTORY_SEPARATOR . 'error.log';
+	
+	// -------
+	
+	if (DIRECTORY_SEPARATOR === '\\')
+		$mydir = str_replace('/', '\\', $mydir);
+	elseif (DIRECTORY_SEPARATOR === '/')
+		$mydir = str_replace('\\', '/', $mydir);
+	
+	if ($maxlen < 6)
+		exit('$maxlen must be at least 6 chars');
+	
+	file_put_contents($errlog, "");
+	
+	while (true)
+	{
+		$dir_iterator = new RecursiveDirectoryIterator($mydir);
+		$iterator     = new RecursiveIteratorIterator($dir_iterator, RecursiveIteratorIterator::SELF_FIRST);
+		
+		$breakit = false;
+		
+		foreach ($iterator as $file)
+		{
+			$file = trim($file);
+			
+			if (is_dir($file))
+			{
+				$dirsplit = explode(DIRECTORY_SEPARATOR, $file);
+				$lastdir  = $dirsplit[ count($dirsplit) - 1 ];
+				
+				if ($lastdir === '.' || $lastdir === '..')
+					continue;
+				
+				unset($dirsplit[ count($dirsplit) - 1 ]);
+				$predir = implode(DIRECTORY_SEPARATOR, $dirsplit);
+				$predir = rtrim($predir, DIRECTORY_SEPARATOR);
+				
+				$nlastdir = substr($lastdir, 0, $maxlen);
+				$nlastdir = preg_replace('/[^äöüÄÖÜa-zA-Z0-9-_., ]/ui', '_', $nlastdir);
+				
+				if (preg_match('/  /ui', $nlastdir))
+					$nlastdir = preg_replace('/ {1,}/ui', ' ', $nlastdir);
+				
+				$nlastdir = trim($nlastdir);
+				
+				$new = $predir . DIRECTORY_SEPARATOR . $nlastdir;
+				
+				if ($file !== $new)
+				{
+					echo $file . "\n";
+					echo '--> ' . $new . "\n";
+					
+					if ($dryrun === false)
+					{
+						$r = rename($file, $new);
+						var_dump($r);
+						
+						if ($r === true)
+						{
+							$breakit = true;
+							
+							//usleep(20000);
+							
+							// DirSchleife neustarten, weil RecursiveIteratorIterator sonst Exception wirft
+							break;
+						}
+						else
+						{
+							file_put_contents($errlog, $file . "\n", FILE_APPEND);
+						}
+					}
+					
+					echo "\n";
+				}
+			}
+			
+		}
+		
+		if ($breakit === false)
+			break;
+	}
   
   
--- scripts/renameFilenames.php
+++ scripts/renameFilenames.php
@@ -1,45 +1,75 @@
 <?php
-  
-  $mydir = 'T:\MP3\_unsortiert';
-  $dryrun = true;
-  
-  $dir_iterator = new RecursiveDirectoryIterator($mydir);
-  $iterator     = new RecursiveIteratorIterator($dir_iterator, RecursiveIteratorIterator::SELF_FIRST);
-
-  foreach ($iterator as $file) 
-  {
-    $file = trim($file);
-    
-    if (is_file($file))
-    {
-      $pi        = pathinfo($file);
-      $dirname   = rtrim($pi['dirname'], DIRECTORY_SEPARATOR);
-      $basename  = $pi['basename'];
-      $extension = $pi['extension'];
-      
-      $nfilename = substr($basename, 0, 64);
-      
-      if (!preg_match('/\.' . preg_quote($extension, '/') . '$/', $nfilename))
-        $nfilename = $nfilename . '.' . $extension;
-      
-      $nfilename = preg_replace('/[^äöüÄÖÜa-zA-Z0-9-_., ]/', '_', $nfilename);
-      $new = $dirname . DIRECTORY_SEPARATOR . $nfilename;
-      
-      if ($file !== $new)
-      {
-        echo $file . "\n";
-        echo '--> ' . $new . "\n";      
-        
-        if ($dryrun === false)
-        {
-          $r = rename($file, $new);
-          var_dump($r);
-          usleep(200000);
-        }
-        
-        echo "\n";
-      }
-    }
-  }
+	
+	if (!isset($mydir))
+		$mydir = 'T:\MP3';
+	if (!isset($dryrun))
+		$dryrun = true;
+	if (!isset($maxlen))
+		$maxlen = 64;
+	if (!isset($errlog))
+		$errlog = __DIR__ . DIRECTORY_SEPARATOR . 'error.log';
+	
+	// -------
+	
+	if (DIRECTORY_SEPARATOR === '\\')
+		$mydir = str_replace('/', '\\', $mydir);
+	elseif (DIRECTORY_SEPARATOR === '/')
+		$mydir = str_replace('\\', '/', $mydir);
+	
+	if ($maxlen < 6)
+		exit('$maxlen must be at least 6 chars');
+	
+	file_put_contents($errlog, "");
+	
+	$dir_iterator = new RecursiveDirectoryIterator($mydir);
+	$iterator     = new RecursiveIteratorIterator($dir_iterator, RecursiveIteratorIterator::SELF_FIRST);
+	
+	foreach ($iterator as $file)
+	{
+		$file = trim($file);
+		
+		if (is_file($file))
+		{
+			$pi        = pathinfo($file);
+			$dirname   = rtrim($pi['dirname'], DIRECTORY_SEPARATOR);
+			$basename  = $pi['basename'];
+			$extension = $pi['extension'];
+			
+			if (empty($extension) || strlen($extension) > 10 || (strlen($extension) > 3 && $maxlen <= 10))
+			{
+				$nfilename = substr($basename, 0, $maxlen);
+			}
+			else
+			{
+				$nfilename = preg_replace('/\.' . preg_quote($extension, '/') . '$/', '', $basename);
+				$nfilename = substr($nfilename, 0, $maxlen - strlen($extension) - 1);
+				$nfilename = $nfilename . '.' . $extension;
+			}
+			
+			$nfilename = preg_replace('/[^äöüÄÖÜa-zA-Z0-9-_., ]/ui', '_', $nfilename);
+			$nfilename = preg_replace('/ {1,}/ui', ' ', $nfilename);
+			
+			$new = $dirname . DIRECTORY_SEPARATOR . $nfilename;
+			
+			if ($file !== $new)
+			{
+				echo $file . "\n";
+				echo '--> ' . $new . "\n";
+				
+				if ($dryrun === false)
+				{
+					$r = rename($file, $new);
+					var_dump($r);
+					
+					if ($r === false)
+						file_put_contents($errlog, $file . "\n", FILE_APPEND);
+					
+					//usleep(20000);
+				}
+				
+				echo "\n";
+			}
+		}
+	}
   
   
